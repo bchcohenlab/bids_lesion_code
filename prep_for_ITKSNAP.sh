ec66b96050122cf396a0bb5d5ef1dc197132e7a1
GillianMiller131 <108811371+GillianMiller131@users.noreply.github.com>
2022-10-13 13:20:59 -0400

    Copy all clinical T1 to output_anat_dir

--- prep_for_ITKSNAP.sh
+++ prep_for_ITKSNAP.sh
@@ -66,9 +66,8 @@ pushd ${output_anat_dir}
 if [ -f "$input_T1w" ]; then
  	cp $input_T1w .
 else
-	echo "*** No single T1w found, making a consolidated one from ax and sag T1w ***"
-	cp ${bids_dir}/${participant}/anat/${participant}_acq-ax_T1w.nii.gz .
-	cp ${bids_dir}/${participant}/anat/${participant}_acq-sag_T1w.nii.gz .
+	echo "*** No single T1w found, making a consolidated one ***"
+	cp ${bids_dir}/${participant}/anat/${participant}_acq-*_T1w.nii.gz .
 	combine_clinical_ax_cor_T1w.sh ${output_anat_dir} ${participant} ax sag
 fi
 
