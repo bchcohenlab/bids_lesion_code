28c643314eeb4f74147da3223a7e41e0c8a9020f
GillianMiller131 <108811371+GillianMiller131@users.noreply.github.com>
2022-10-21 15:21:04 -0400

    Cont. input standardization & add modules for e2

--- segment_with_DeepNeuro.sh
+++ segment_with_DeepNeuro.sh
@@ -7,30 +7,34 @@
 bids_dir=$1
 participant=$2
 
+module load cuda
+module load singularity
+source /programs/biogrids.shrc
+
 BIDSPATH=${bids_dir}/code/bids_lesion_code
 
 # Expected input locations:
 b0=${participant}_space-T1w_desc-b0_dwi.nii.gz # This is the b0 image
 b1000=${participant}_space-T1w_desc-b1000_dwi.nii.gz # This is the b1000 image
+input_dir=${bids_dir}/derivatives/lesions/${participant}/dwi 
 
 # Output location:
 output_dir=${bids_dir}/derivatives/lesions/${participant}/DeepNeuro
 mkdir -p ${output_dir}
 
-cp ${working_dir}/${b0} ${output_dir}
-cp ${working_dir}/${b1000} ${output_dir}
+cp ${input_dir}/${b0} ${output_dir}
+cp ${input_dir}/${b1000} ${output_dir}
 
 # Run the DeepNeuro docker (requires CUDA)
-singularity exec \
+singularity exec --nv \
 	-B ${output_dir}:/INPUT_DATA \
-	--nv
 	${BIDSPATH}/deepneuro.sif \
 	segment_ischemic_stroke pipeline \
 	-B0 /INPUT_DATA/${b0} \
 	-DWI /INPUT_DATA/${b1000} \
 	-output_folder /INPUT_DATA \
 	-registered
-
+	
 #pushd ${output_dir}
 #	for i in `find . -type f -name "*.gz"`; do 
 #		echo converting $i to float32
