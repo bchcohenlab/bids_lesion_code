e1ba75ac4ac6af52337f6687fcaf99e52b5c2195
GillianMiller131 <108811371+GillianMiller131@users.noreply.github.com>
2022-12-14 12:43:23 -0500

    Update to allow cases of 3 clinical T1w

--- combine_clinical_ax_cor_T1w.sh
+++ combine_clinical_ax_cor_T1w.sh
@@ -14,26 +14,29 @@
 
 working_dir=$1
 participant=$2
-target_T1w_acq=$3
-other_T1w_acq=$4
+A_T1w_acq=$3
+B_T1w_acq=$4
+C_T1w_acq=$5
 
-template=${working_dir}/${participant}_acq-${target_T1w_acq}_T1w.nii.gz # This is the target T1w
-orig=${working_dir}/${participant}_acq-${other_T1w_acq}_T1w.nii.gz # This is another T1w
+first=${working_dir}/${participant}_${A_T1w_acq}_T1w.nii.gz # This is the target T1w
+second=${working_dir}/${participant}_${B_T1w_acq}_T1w.nii.gz # Another T1w
+third=${working_dir}/${participant}_${C_T1w_acq}_T1w.nii.gz # 3rd T1w if applicable 
 
 #### High quality fusion using niftymic:
 
 # First, Make binary masks of where the two images have values:
+if [ -z "$5" ]; then 
 fslmaths \
-	${template} \
+	${first} \
 	-abs \
 	-bin \
-	temp_${target_T1w_acq}_mask
+	temp_${A_T1w_acq}_mask
 
 fslmaths \
-	${orig} \
+	${second} \
 	-abs \
 	-bin \
-	temp_${other_T1w_acq}_mask
+	temp_${B_T1w_acq}_mask
 
 
 singularity exec \
@@ -42,11 +45,46 @@ singularity exec \
 	${BIDSPATH}/niftymic.sif \
 	niftymic_reconstruct_volume \
 		--filenames \
-			/app/data/${participant}_acq-${target_T1w_acq}_T1w.nii.gz \
-			/app/data/${participant}_acq-${other_T1w_acq}_T1w.nii.gz \
+			/app/data/${participant}_acq-${A_T1w_acq}_T1w.nii.gz \
+			/app/data/${participant}_acq-${B_T1w_acq}_T1w.nii.gz \
 		--filenames-masks \
-			/app/data/temp_${target_T1w_acq}_mask.nii.gz \
-			/app/data/temp_${other_T1w_acq}_mask.nii.gz \
+			/app/data/temp_${A_T1w_acq}_mask.nii.gz \
+			/app/data/temp_${B_T1w_acq}_mask.nii.gz \
+		--output \
+			/app/data/${participant}_T1w.nii.gz
+elif [ "$5" ]; then
+fslmaths \
+	${first} \
+	-abs \
+	-bin \
+	temp_${A_T1w_acq}_mask
+
+fslmaths \
+	${second} \
+	-abs \
+	-bin \
+	temp_${B_T1w_acq}_mask
+
+fslmaths \
+	${third} \
+	-abs \
+	-bin \
+	temp_${C_T1w_acq}_mask
+
+
+singularity exec \
+	-B ${working_dir}:/app/data \
+        -B ${BIDSPATH}:${BIDSPATH} \
+	${BIDSPATH}/niftymic.sif \
+	niftymic_reconstruct_volume \
+		--filenames \
+			/app/data/${participant}_${A_T1w_acq}_T1w.nii.gz \
+			/app/data/${participant}_${B_T1w_acq}_T1w.nii.gz \
+			/app/data/${participant}_${C_T1w_acq}_T1w.nii.gz \
+		--filenames-masks \
+			/app/data/temp_${A_T1w_acq}_mask.nii.gz \
+			/app/data/temp_${B_T1w_acq}_mask.nii.gz \
+			/app/data/temp_${C_T1w_acq}_mask.nii.gz \
 		--output \
 			/app/data/${participant}_T1w.nii.gz
 
