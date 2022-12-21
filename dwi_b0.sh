e71994b123e7936d5e0588cb60e505e8b2521819
GillianMiller131 <108811371+GillianMiller131@users.noreply.github.com>
2022-12-21 13:30:28 -0700

    Update $1 to $dwi

--- dwi_b0.sh
+++ dwi_b0.sh
@@ -5,7 +5,7 @@
 dwi=$1
 participant=`basename $dwi | cut -d _ -f 1`
 
-fslroi $1 ${participant}_desc-b0_dwi.nii.gz 0 1
+fslroi $dwi ${participant}_desc-b0_dwi.nii.gz 0 1
 
 ((last_frame=`fslval $dwi dim4` - 1))
-fslroi $1 ${participant}_desc-b1000_dwi.nii.gz ${last_frame} 1
+fslroi $dwi ${participant}_desc-b1000_dwi.nii.gz ${last_frame} 1
