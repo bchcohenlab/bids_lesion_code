302b97de8ddaecde4585c7a054cb7160ed0aa45a
GillianMiller131 <108811371+GillianMiller131@users.noreply.github.com>
2023-05-10 12:26:13 -0400

    update bc b1000 not always last frame

--- dwi_b0.sh
+++ dwi_b0.sh
@@ -4,8 +4,20 @@
 
 dwi=$1
 participant=`basename $dwi | cut -d _ -f 1`
+dwi_name=`echo $dwi| cut -d '.' -f1`
+bval=${dwi_name}.bval
 
 fslroi $dwi ${participant}_desc-b0_dwi.nii.gz 0 1
 
-((last_frame=`fslval $dwi dim4` - 4))
-fslroi $dwi ${participant}_desc-b1000_dwi.nii.gz ${last_frame} 1
+#((last_frame=`fslval $dwi dim4` - 1))
+
+b1000_frame=0
+for i in `cat $bval`; do
+    if [ $i == 1000 ]; then
+        break
+    else
+        ((b1000_frame++))
+    fi
+done
+
+fslroi $dwi ${participant}_desc-b1000_dwi.nii.gz ${b1000_frame} 1
